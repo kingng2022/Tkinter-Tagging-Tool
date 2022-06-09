869613eb38387ff0bd4427e1db56e16c3b47520c
kingng2022 <107106242+kingng2022@users.noreply.github.com>
2022-06-09 15:15:24 +0800

    added undo function

--- cleanliness_tagging.py
+++ cleanliness_tagging.py
@@ -6,6 +6,7 @@ import re
 import os
 import copy
 
+
 #counter for image labelled 
 class counter:
     def __init__(self, count):
@@ -13,11 +14,13 @@ class counter:
 
 n = counter(0)
 
+
 #params for grids spliting 
 min_y, max_y, min_x, max_x = 350, 650, 150, 1650
 color = (0,255,0)
 thickness = 2
 grid_w, grid_h = 100, 100
+
 
 #calculate the grid positions 
 grid_position = [] 
@@ -25,9 +28,11 @@ for w in range(min_x, max_x, grid_w):
     for h in range(min_y, max_y, grid_h):
         grid_position.append([(w,h),(w+grid_w,h+grid_h)])
 
+        
 #sort images in the folder 
 image_path = glob.glob('*.jpg')
 image_path.sort(key=lambda x:(int(re.search('(\d+)-(\d+)_(\d+)_(\d+).jpg', x).group(1)),int(re.search('(\d+)-(\d+)_(\d+)_(\d+).jpg', x).group(2)),int(re.search('(\d+)-(\d+)_(\d+)_(\d+).jpg', x).group(3)),int(re.search('(\d+)-(\d+)_(\d+)_(\d+).jpg', x).group(4)) ))
+
 
 #name and create target  folders 
 target_dir = ['clean_wet', 'clean-half', 'clean_dry', 'little-dirty', 'sparse-dirty', 'dense-dirty', 'blockage', 'broader']
@@ -37,6 +42,8 @@ for d in target_dir:
         os.mkdir(d)
     except:
         pass
+    
+    
 #create the history list recording the files movement
 history = []
 
@@ -58,6 +65,7 @@ def display_crop():
     panel_crop.configure(image = img_crop)
     panel_crop.image = img_crop
 
+    
 #button functions
 def move2file(target_path):
     shutil.move(image_path[n.count],target_path+ '/'+ image_path[n.count]) 
@@ -68,7 +76,6 @@ def move2file(target_path):
     display_full()
     display_crop()
  
-
 def undo():
     try:
         shutil.move(history[-1][1]+'/'+history[-1][0], history[-1][0])
@@ -80,6 +87,7 @@ def undo():
     except:
         print('nothing to undo!')
 
+        
 #create the tool window
 win =  tk.Tk()
 win.geometry("1600x900")
@@ -87,26 +95,24 @@ win.geometry("1600x900")
 #buttons params
 btn_w = 15
 btn_h = 5
-
-#class buttons
 btn_list = [tk.Button(win)]*len(target_dir)
 relx = [0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85]
 rely = [0.2]*len(target_dir)
 hkey = ['a','s','d','f','h','j','k','l']
 
+#create class buttons
 for i in range(len(btn_list)):
     btn_list[i]  = tk.Button(win, text=target_dir[i]+f'({hkey[i].upper()})', width=btn_w, height=btn_h, command= lambda m=target_dir[i]: move2file(m))
     btn_list[i].place(relx=relx[i] ,rely= rely[i], anchor='center')
     win.bind(hkey[i], lambda event, d = target_dir[i]: move2file(d))
 
-#undo button 
+#create undo button 
 btn_undo  = tk.Button(win, text='Undo(U)', width=btn_w, height=btn_h, command= lambda: undo())
 btn_undo.place(relx=0.95 ,rely= 0.05, anchor='center')
 win.bind('u', lambda event: undo()) 
 
-
 #images display
-#original images with grid
+#original images with grids
 img_path = re.search('(.+)_\d+.jpg',image_path[n.count]).group(1)
 img = Image.open(f'../cropped_image/org_{img_path}.jpg')
 img = ImageTk.PhotoImage(img)
